21290e5a2aed3a5bf33e0895235fcf33d6dfa0a5
yapijs <daviszarins90@gmail.com>
2022-10-20 18:30:07 +0300

    filereader, tests

---
+++ src/main/java/io/test_app/DictionaryApp.java
@@ -0,0 +1,12 @@
+package io.test_app;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class DictionaryApp {
+    public static void main(String[] args) throws URISyntaxException, IOException {
+
+
+
+    }
+}
